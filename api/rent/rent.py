2e7fb42866e6be4265cd814be904b5f1c2365565
PlugDev77 <ochh508@naver.com>
2022-09-18 22:42:34 +0900

    대여 물품 임시 API 적용

---
+++ api/rent/rent.py
@@ -0,0 +1,101 @@
+import requests
+from flask import Flask, redirect, request
+from flask_restx import Resource, Api, Namespace
+
+rent = Namespace('rent')
+
+@rent.route("/list")
+class RentList(Resource):
+    def get(self):
+        data = [
+            {
+                "id": 1,
+                "deadline": "2022-11-01",
+                "d_day": 50,
+                "rent_day": "2022-09-10",
+                "product": {
+                    "id": 60,
+                    "is_available": False,
+                    "name": "테스트 물품",
+                    "category": "도서", # String,
+                    "location": "A-123", # String,
+                    "status": "대여중", # String,
+                    "author": "김판큐", # String?,
+                    "publisher": "판큐출판사", # String?
+                },
+            },
+            {
+                "id": 2,
+                "deadline": "2022-11-01",
+                "d_day": 50,
+                "rent_day": "2022-09-10",
+                "product": {
+                    "id": 60,
+                    "is_available": False,
+                    "name": "테스트 물품",
+                    "category": "도서", # String,
+                    "location": "A-123", # String,
+                    "status": "대여중", # String,
+                    "author": "김판큐", # String?,
+                    "publisher": "판큐출판사", # String?
+                },
+            },
+            {
+                "id": 3,
+                "deadline": "2022-11-01",
+                "d_day": 50,
+                "rent_day": "2022-09-10",
+                "product": {
+                    "id": 60,
+                    "is_available": False,
+                    "name": "테스트 물품",
+                    "category": "도서", # String,
+                    "location": "A-123", # String,
+                    "status": "대여중", # String,
+                    "author": "김판큐", # String?,
+                    "publisher": "판큐출판사", # String?
+                },
+            },
+        ]
+        return data
+
+
+@rent.route("/<int:product_id>")
+class RentProduct(Resource):
+    def get(self, product_id):
+        data1 = {
+            "id": 1,
+            "deadline": "2022-11-01",
+            "d_day": 50,
+            "rent_day": "2022-09-10",
+            "product": {
+                "id": 60,
+                "is_available": False,
+                "name": "테스트 도서_이용불가",
+                "category": "도서", # String,
+                "location": "A-123", # String,
+                "status": "대여중", # String,
+                "author": "김판큐", # String?,
+                "publisher": "판큐출판사", # String?
+            },
+        }
+        data2 = {
+            "id": 1,
+            "deadline": "2022-11-01",
+            "d_day": 50,
+            "rent_day": "2022-09-10",
+            "product": {
+                "id": 60,
+                "is_available": True,
+                "name": "테스트 물품_이용가능",
+                "category": "타블렛", # String,
+                "location": "A-123", # String,
+                "status": "대여 가능", # String,
+                "author": None, # String?,
+                "publisher": None, # String?
+            },
+        }
+        if product_id == 1:
+            return data1
+        else:
+            return data2
--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_restx import Api
 from api.auth.oauth import oauth
 from api.user.user import user
+from api.rent.rent import rent
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_namespace(oauth, '/oauth')
 api.add_namespace(user, '/user')
+api.add_namespace(rent, '/rent')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
