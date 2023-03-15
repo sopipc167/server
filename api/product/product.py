1eaa13e2b8e6599fde1cb80d4731d61b19adc9f0
Changhan Oh <changhan@Changhans-MacBook-Pro.local>
2023-03-15 14:18:40 +0900

    물품 대여 API 구현 완료
    추후 JWT 적용 이후에는 조금 변경될 가능성 있음

--- api/product/product.py
+++ api/product/product.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_restx import Resource, Namespace
 from database.database import Database
+from datetime import datetime, timedelta
 
 product = Namespace('product')
 
@@ -45,26 +46,54 @@ class SpecificProductList(Resource):
             return message, 400
         else:
             return product_list, 200
-'''
+
 # 구현중
 @product.route("/rent/<string:product_code>")
 class RentProduct(Resource):
-    def put(self, product_code):
+    def post(self, product_code):
         database = Database()
         sql = f"SELECT * FROM products where code = {product_code}"
         product = database.execute_one(sql)
         
-        if product['is_available']:
-            # 물품에 대한 내용
-            sql = f"UPDATE products SET is_available = {False} WHERE code = {product_code};"
+        if product['is_available']: # 물품 대여에 대한 로직
+            # 물품 정보를 대여중인 상태로 업데이트
+            status = "대여중"
+            sql = f"UPDATE products SET is_available = {False}, status = {status} WHERE code = {product_code};"
             database.execute(sql)
             
-            # 반납 기한, 디데이, 대여한 날짜 설정
-            sql = f"UPDATE rent_list SET is_available = {False} WHERE code = {product_code};"
+            # 물품 대여 내역 추가
+            now = datetime.now()
+            rent_day = now.date()
+            deadline = rent_day + timedelta(30)
+            status_value = 0
+            sql = f"INSERT INTO rent_list VALUES({product_code}, {1}, {deadline}, {rent_day}, {status_value});"
+            database.execute(sql)
+
+            # 물품 정보가 변경 되었으므로 물품 상세 정보 재조회
+            sql = f"SELECT * FROM products where code = {product_code};"
+            product_data = database.execute_one(sql)
+
+            # 빌린 사람 이름 조회
+            sql = f"SELECT name FROM users where id = {2}"
+            rent_user = database.execute_one(sql)
+
+            # 디데이 계산
+            d_day = (deadline - rent_day).days
+
+            # 물품 정보에 대여 관련 정보 추가
+            product_data['deadline'] = deadline.strftime('%Y/%m/%d')
+            product_data['rent_day'] = rent_day.strftime('%Y/%m/%d')
+            product_data['d_day'] = d_day
+            product_data['status'] = { 'value': '대여중', 'rent_user': rent_user['name'] }
+
             database.close()
-            return {}, 200
+            return product_data, 200
         else:
             database.close()
             message = { 'message': '현재 대여가 불가능한 물품이에요 :(' }
             return message, 400
-'''
+
+@product.route("/return/<string:product_code>")
+class ReturnProduct(Resource):
+    def get(self, product_code):
+        return {}, 200
