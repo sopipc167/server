ddbb2dd852c742918f83bce9d7e4446f048e9fd7
Changhan Oh <changhan@Changhans-MacBook-Pro.local>
2023-03-28 22:13:22 +0900

    프로젝트 목록 API와 유저 프로필 API의 DTO 수정

--- api/user/user.py
+++ api/user/user.py
@@ -18,6 +18,9 @@ class UserProfile(Resource):
         if not user_data:
             database.close()
             return { 'message': '회원 정보를 찾지 못했어요 :(' }, 400
+        
+        # 동아리에 가입되어 있는 여부를 Boolean 값으로 변경
+        user_data['is_signed'] = True if user_data['is_signed'] else False
 
         # 소속된 프로젝트(들)의 식별자 목록 조회
         sql = f"SELECT project_id FROM project_members where user_id = {user_data['id']};"
@@ -31,22 +34,43 @@ class UserProfile(Resource):
             project_data = database.execute_one(sql)
 
             # Date를 String으로 형 변환함
-            project_data['start_date'] = project_data['start_date'].strftime('%Y/%m/%d')
-            project_data['end_date'] = project_data['end_date'].strftime('%Y/%m/%d')
+            project_data['start_date'] = project_data['start_date'].strftime('%Y-%m-%d')
+            project_data['end_date'] = project_data['end_date'].strftime('%Y-%m-%d')
+
+            # 플랫폼 정보를 List<String>의 형식으로 변환
+            platform_list = project_data['platform'].split(',')
+            project_data['platform'] = platform_list
+
+            # 팀원 모집 여부와 문의 가능 여부를 Boolean 값으로 변경
+            project_data['is_finding_member'] = True if project_data['is_finding_member'] else False
+            project_data['is_able_inquiry'] = True if project_data['is_able_inquiry'] else False
+
+            # PM 초기화
+            project_data['pm'] = None
 
             # 소속된 프로젝트(들)의 멤버 식별자 목록 조회
             sql = f"SELECT * FROM project_members where project_id = {id};"
-            project_member_id_list = database.execute_all(sql)
+            project_members = database.execute_all(sql)
             
-            # 멤버 식별자 목록을 이용하여 각 멤버별 상세 정보 조회
+            # 프로젝트 팀원 별 상세 정보를 불러오고 members와 pm에 각각 할당
             members = []
-            for member_id in project_member_id_list:
-                id = member_id['user_id']
+            for member in project_members:
+                # 유저 상세 정보 조회
+                id = member['user_id']
                 sql = f"SELECT * FROM users where id = {id};"
                 member_data = database.execute_one(sql)
-                members.append(member_data)
+
+                # 동아리에 가입되어 있는 여부를 Boolean 값으로 변경
+                member_data['is_signed'] = True if member_data['is_signed'] else False
+
+                # PM이면 pm에 따로 추가, PM이 아니면 members에 추가
+                if member['is_pm']:
+                    project_data['pm'] = member_data
+                else:
+                    members.append(member_data)
             project_data['members'] = members
             
+            # 얻은 프로젝트 정보를 리스트에 추가
             project_data_list.append(project_data)
         
         user_data['projects'] = project_data_list
