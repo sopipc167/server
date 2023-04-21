6727e44621e58d49d576a5bbfadf186dcc1a7b7a
rhdehgks <rhdehgks@naver.com>
2023-04-21 19:26:33 +0900

    response status code 수정

--- api/seminar/seminar.py
+++ api/seminar/seminar.py
@@ -7,6 +7,7 @@ seminar = Namespace('seminar')
 @seminar.route('/<int:user_id>')
 class SeminarUserAPI(Resource):
     def get(self, user_id):
+        
         #데이터베이스에서 user_id값에 맞는 세미나 목록 불러오기
         database = Database()
         sql = f"select * from seminars where user_id = {user_id}"
@@ -19,4 +20,4 @@ class SeminarUserAPI(Resource):
             for idx, seminar in enumerate(seminar_list):
                 # 날짜를 문자열로 변경
                 seminar_list[idx]['date'] = seminar['date'].strftime('%Y-%m-%d')
-            return seminar_list, 400
+            return seminar_list, 200
