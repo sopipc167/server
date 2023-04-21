6321eddd25211dce6d9bb3232677f271f427940e
rhdehgks <rhdehgks@naver.com>
2023-04-21 19:08:44 +0900

    세미나 API 추가

---
+++ api/seminar/seminar.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from flask_restx import Resource, Namespace
+from database.database import Database
+
+seminar = Namespace('seminar')
+
+@seminar.route('/<int:user_id>')
+class SeminarUserAPI(Resource):
+    def get(self, user_id):
+        #데이터베이스에서 user_id값에 맞는 세미나 목록 불러오기
+        database = Database()
+        sql = f"select * from seminars where user_id = {user_id}"
+        seminar_list = database.execute_all(sql)
+
+        if not seminar_list: # 세미나를 한 적이 없을 때 처리
+            database.close()
+            return [], 200
+        else:
+            for idx, seminar in enumerate(seminar_list):
+                # 날짜를 문자열로 변경
+                seminar_list[idx]['date'] = seminar['date'].strftime('%Y-%m-%d')
+            return seminar_list, 400
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from api.auth.oauth import oauth
 from api.user.user import user
 from api.product.product import product
 from api.project.project import project
+from api.seminar.seminar import seminar
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +13,7 @@ api.add_namespace(oauth, '/oauth')
 api.add_namespace(user, '/user')
 api.add_namespace(product, '/product')
 api.add_namespace(project, '/project')
+api.add_namespace(seminar, '/seminar')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
