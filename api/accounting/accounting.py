8a150c342b80731c4965898c3802d06b1b92d55e
rhdehgks <rhdehgks@naver.com>
2023-05-26 20:59:43 +0900

    스케줄러를 고려한 코드 수정 및 계좌 전체 금액에 대한 API 통합

--- api/accounting/accounting.py
+++ api/accounting/accounting.py
@@ -1,136 +1,105 @@
 from flask import Flask, request
 from flask_restx import Resource, Namespace
 from database.database import Database
-from datetime import datetime
+from datetime import datetime, date
 
 accounting = Namespace('accounting')
 
-# category(내역 유형)
+# 회비 category
+MEMBERSHIP_FEE_CATEGORY = {0: '기간 외', 1: '납부 완료', 2: '납부 지연', 3: '미납'}
+
+# 회계 내역 category(내역 유형)
 ACCOUNTING_CATEGORY = {0: '문의 불가', 1: '문의 가능'}
 
 # payment_method(결제 수단)
 PAYMENT_METHOD = {0: '통장', 1: '금고'}
 
-# category를 문자열로 변환
-def ac_int_to_str(category):
-    return ACCOUNTING_CATEGORY.get(category, None)
+# index 데이터를 문자열로 변경
+def convert_to_string(dictionary, index):
+    return dictionary.get(index, None)
 
-# category를 index로 변환
-def ac_str_to_int(category):
-    for key, value in ACCOUNTING_CATEGORY.items():
-        if value == category:
+# 문자열 데이터를 index로 변경
+def convert_to_index(dictionary, string):
+    for key, value in dictionary.items():
+        if value == string:
             return key
     return None
 
-# payment_method를 문자열로 변환
-def pm_int_to_str(payment_method):
-    return PAYMENT_METHOD.get(payment_method, None)
-
-# payment_method를 index로 변환
-def pm_str_to_int(payment_method):
-    for key, value in PAYMENT_METHOD.items():
-        if value == payment_method:
-            return key
-    return None
-
-# 납부 상태 반환
-def get_payment_state(monthly_payment):
-    if monthly_payment['payment_date']:
-        if monthly_payment['payment_date'] <= monthly_payment['end_day']:
-            return "납부 완료"
-        else:
-            return "납부 지연"
-    else:
-        current_date = datetime.now().date()
-        if current_date < monthly_payment['start_day']:
-            return "기간 외"
-        else:
-            return "미납"
-        
-# 회원 등급 및 학년에 따른 회비 반환
-def get_amount(user_info):
-    if user_info['level'] == 0 and user_info['grade'] == 4: 
-        return 3000
-    else:
-        return 5000
+# DB에있는 모든 내역의 금액의 합 계산하기
+def get_total_amount(database):
+    sql = "SELECT sum(amount) as total FROM accountings" 
+    accounting = database.execute_one(sql)
+    return int(accounting['total'])
 
 @accounting.route('/<int:user_id>')
 class AccountingUserAPI(Resource):
     # 회원의 월별 회비 납부 내역 얻기
     def get(self, user_id):
+        # 금월 및 작년 6월 날짜 구한 뒤 문자열로 변환 ('YYYY-MM-01' 형식)
+        current_month = date(datetime.today().year, datetime.today().month, 1)
+        start_month = date(current_month.year - 1, 6, 1)
+        current_month.strftime('%Y-%m-%d')
+        start_month.strftime('%Y-%m-%d')
+
+        print(current_month, start_month)
+
         database = Database()
 
-        # DB에서 user_id값에 맞는 월별 회비 납부 내역 불러오기
-        sql = f"SELECT mpp.date, start_day, end_day, mf.date as payment_date, amount "\
-            f"FROM monthly_payment_periods mpp "\
-            f"LEFT JOIN membership_fees mf "\
-            f"ON mf.user_id = {user_id} and year(mpp.date) = year(mf.date) and month(mpp.date) = month(mf.date) "\
-            f"ORDER BY mpp.date;"
+        # DB에서 user_id값에 맞는 월별 회비 납부 내역 불러오기 (작년 6월부터 현재 달까지)
+        sql = f"SELECT date, amount, category FROM membership_fees "\
+            f"WHERE user_id = {user_id} "\
+            f"and date between '{start_month}' and '{current_month}'"
         monthly_payment_list = database.execute_all(sql)
 
-        # 납부해야 할 회비를 확인하기 위해 필요한 회원 정보 불러오기
-        sql = f"SELECT level, grade FROM users WHERE id = {user_id};"
-        user_info = database.execute_one(sql)
-        # 납부해야 할 회비 금액
-        amount = get_amount(user_info)
+        # 금월 회비 납부 기간 불러오기
+        sql = f"SELECT start_day, end_day FROM monthly_payment_periods "\
+            f"WHERE date = '{current_month}'"
+        payment_period = database.execute_one(sql)
+        
+        # 계좌 내의 총 금액 불러오기
+        total_amount = get_total_amount(database)
+
         database.close()
 
-        # 입부 전(납부를 처음으로 시작한 달 이전엔 아직 입부하지 않은 것으로 간주)의 데이터 제거
-        filtered_list = []
-        for monthly_payment in monthly_payment_list:
-            if monthly_payment['payment_date'] or filtered_list:
-                filtered_list.append(monthly_payment)
-        monthly_payment_list = filtered_list
+        # 납부 기간 문자열로 변환
+        if payment_period:
+            payment_period['start_day'] = payment_period['start_day'].strftime('%Y-%m-%d')
+            payment_period['end_day'] = payment_period['end_day'].strftime('%Y-%m-%d')
+
+        result_data = {'monthly_payment_list': monthly_payment_list, 'payment_period': payment_period, 'total_amount': total_amount}
 
         if not monthly_payment_list: # 납부 내역이 없을 때의 처리
-            return [], 200
+            return result_data, 200
         else:
             for idx, monthly_payment in enumerate(monthly_payment_list):
-                # 납부 상태 설정
-                monthly_payment_list[idx]['state'] = get_payment_state(monthly_payment)
-
-                # 납부 금액 설정
-                if not monthly_payment_list[idx]['amount']:
-                    monthly_payment_list[idx]['amount'] = amount
-
                 # 각 날짜를 문자열로 변환
                 monthly_payment_list[idx]['date'] = monthly_payment['date'].strftime('%Y-%m-%d')
-                monthly_payment_list[idx]['start_day'] = monthly_payment['start_day'].strftime('%Y-%m-%d')
-                monthly_payment_list[idx]['end_day'] = monthly_payment['end_day'].strftime('%Y-%m-%d')
-                if monthly_payment_list[idx]['payment_date']:
-                    monthly_payment_list[idx]['payment_date'] = monthly_payment['payment_date'].strftime('%Y-%m-%d')
 
-            return monthly_payment_list, 200
-        
-@accounting.route('/total')
-class AccountingTotalAPI(Resource):
-    # 현재 계좌 내의 총 금액 얻기
-    def get(self):
-        # DB에있는 모든 내역의 금액의 합 계산하기
-        database = Database()
-        sql = "SELECT sum(amount) as total FROM accountings" 
-        accounting = database.execute_one(sql)
-        database.close()
-        accounting['total'] = int(accounting['total'])
-
-        return accounting, 200
+            return result_data, 200
     
 @accounting.route('/list')
 class AccountingListAPI(Resource):
     # 회비 내역 목록 얻기
     def get(self):
-        # DB에서 전체 회비 내역 목록 불러오기
         database = Database()
+
+        # DB에서 전체 회비 내역 목록 불러오기
         sql = "SELECT * FROM accountings"
         accounting_list = database.execute_all(sql)
+
+        # 계좌 내의 총 금액 불러오기
+        total_amount = get_total_amount(database)
+
         database.close()
 
+        result_data = {'accounting_list': accounting_list, 'total_amount': total_amount}
+
         if not accounting_list: # 회비 내역이 없을 때 처리
-            return [], 200
+            return result_data, 200
         else:
             for idx, accounting in enumerate(accounting_list):
                 # date 및 category, payment_method를 문자열로 변환
                 accounting_list[idx]['date'] = accounting['date'].strftime('%Y-%m-%d')
-                accounting_list[idx]['category'] = ac_int_to_str(accounting['category'])
-                accounting_list[idx]['payment_method'] = pm_int_to_str(accounting['payment_method'])
-            return accounting_list, 200
+                accounting_list[idx]['category'] = convert_to_string(ACCOUNTING_CATEGORY, accounting['category'])
+                accounting_list[idx]['payment_method'] = convert_to_string(PAYMENT_METHOD, accounting['payment_method'])
+            return result_data, 200
