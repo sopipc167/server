06c7f08b038690483986432cc0fdf051e829371d
Changhan Oh <ochh508@naver.com>
2023-08-23 00:44:29 +0900

    AES 암호화 로직 변경

--- utils/aes_cipher.py
+++ utils/aes_cipher.py
@@ -1,40 +1,32 @@
 import base64
 import hashlib
-
-from Cryptodome import Random
 from Cryptodome.Cipher import AES
-from Cryptodome.Util.Padding import pad, unpad
-
 import configparser
 
 config = configparser.ConfigParser()
 config.read_file(open('config/config.ini'))
 SECRET_KEY = config['database']['encryption_key']
 
-class AESCipher:
-    def __init__(self):
-        self.BS = 16
-        self.key = hashlib.sha256(SECRET_KEY.encode('utf-8')).digest()
-        self.iv = Random.new().read(AES.block_size)
+BS = 16
+pad = (lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS).encode())
+unpad = (lambda s: s[:-ord(s[len(s)-1:])])
 
-    def encrypt(self, raw):
-        raw = pad(raw.encode('utf-8'), AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        return base64.b64encode(cipher.encrypt(raw))
-        
+class AESCipher(object):
+    def __init__(self):
+        self.key = hashlib.sha256(SECRET_KEY.encode()).digest()
+    
+    def encrypt(self, message):
+        message = message.encode()
+        raw = pad(message)
+        cipher = AES.new(self.key, AES.MODE_CBC, self.__iv().encode('utf8'))
+        enc = cipher.encrypt(raw)
+        return base64.b64encode(enc).decode('utf-8')
+    
     def decrypt(self, enc):
         enc = base64.b64decode(enc)
-        cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        return unpad(cipher.decrypt(enc), AES.block_size)
-
-    def encrypt_str(self, raw):
-        if raw is None or raw == '':
-            return None
-        return self.encrypt(raw).decode('utf-8')
-
-    def decrypt_str(self, enc):
-        if enc is None or enc == '':
-            return None
-        if type(enc) == str:
-            enc = str.encode(enc)
-        return self.decrypt(enc).decode('utf-8')
+        cipher = AES.new(self.key, AES.MODE_CBC, self.__iv().encode('utf8'))
+        dec = cipher.decrypt(enc)
+        return unpad(dec).decode('utf-8')
+    
+    def __iv(self):
+        return chr(0) * 16
