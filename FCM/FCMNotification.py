36789f2a2d91336956e2190f90ab877ef5edf91a
sopipc167 <sopipc167@gmail.com>
2023-09-17 18:08:52 +0900

    메세지가 만료되었을시 메세지 객체 삭제

--- FCM/FCMNotification.py
+++ FCM/FCMNotification.py
@@ -5,18 +5,25 @@ import datetime
 import time
 
 class Notification: #알림 객체
-    DAY_OF_WEEK={'월요일':'monday','화요일':'tuesday','수요일':'wendsday','목요일':'thrsday','금요일':'frieday','토요일':'saturday','일요일':'sunday'} #알림의 요일을 영어로 바꾸기 위한 딕셔너리
-    scheduler=""
-    expire_day=datetime.datetime
+    DAY_OF_WEEK = {'월요일':'monday','화요일':'tuesday','수요일':'wendsday','목요일':'thrsday','금요일':'frieday','토요일':'saturday','일요일':'sunday'} #알림의 요일을 영어로 바꾸기 위한 딕셔너리
+    scheduler = ""
+    expire_day = datetime.datetime
+    start_day = datetime.datetime
+    week = ''
+    time = time.time()
+    cycle = ''
     def __init__(self, data, apikey, token): #API Key와 앱 토큰을 초기화
         self.DATA = data
         APIKEY = apikey
         TOKEN = token
         push_service = FCMNotification(APIKEY)
         self.timeParser(data)
+    def __del__(self):
+        print(f'{self.cycle} {self.time}시에 예약된 알람을 삭제했습니다.')
 
-    def sendMessage(self,body, title):
+    async def sendMessage(self,body, title):
         if datetime.datetime.now()<self.expire_day: #만약 날짜가 만료되면 자동으로 스케쥴 취소
+            self.__del__()
             return schedule.CancelJob
         # 메시지 (data 타입)
         data_message = {
@@ -25,20 +32,22 @@ class Notification: #알림 객체
         }
         result = self.push_service.single_device_data_message(registration_id=self.TOKEN, data_message=data_message)# 토큰값을 이용해 1명에게 푸시알림을 전송함
         print(result) # 전송 결과 출력
+
     def startSchedule(self): #스케쥴을 시작함
         eval(self.scheduler)
+
     def setSchedule(self, data): #스케쥴을 설정하여 몇일, 몇주 주기로 메세지를 보낼지 결정
         for i in data:
-            time=i['time']
-            week=i['day']
-            start_date=i['start_date']
+            self.time=i['time']
+            self.week=i['day']
+            self.start_date=i['start_date']
             self.expire_day=i['end_date']
-            cycle = i['cyle']
+            self.cycle = i['cyle']
             message=i['message']
             memo=i['memo']
-        if(cycle=='매주'):
-            self.scheduler = f"schedule.every().{self.DAY_OF_WEEK[week]}.at(\"{time}\").do(self.sendMessage,{message},{memo})"
-        elif(cycle=='매일'):
+        if(self.cycle=='매주'):
+            self.scheduler = f"schedule.every().{self.DAY_OF_WEEK[self.week]}.at(\"{self.time}\").do(self.sendMessage,{message},{memo})"
+        elif(self.cycle=='매일'):
             self.scheduler = f"schedule.every().day.at(\"{time}\").do(self.sendMessage,{message},{memo})"
         else:
             self.scheduler = None
--- FCM/NotificationManager.py
+++ FCM/NotificationManager.py
@@ -1,9 +1,12 @@
 import schedule
 from database.database import Database
 from FCM.FCMNotification import Notification
+from pyfcm import FCMNotification
+import firebase_admin
+
+default_app = firebase_admin.initialize_app()
 
 FCM_LIST =[]
-
 def fetch_data(): #데이터베이스에서 알림 정보를 가져와서 알림 객체를 생성함
     database=Database()
     lst = []
@@ -12,7 +15,25 @@ def fetch_data(): #데이터베이스에서 알림 정보를 가져와서 알림
     database.close()
     for i in lst:
         tmp=Notification(i,'unknown string wanted','unknown string wanted')
+        tmp.startSchedule()
         FCM_LIST.append(tmp)
+def delete_notification():
+    pass
+def new_topic():
+    registration_tokens = [
+        'YOUR_REGISTRATION_TOKEN_1',
+        # ...
+        'YOUR_REGISTRATION_TOKEN_n',
+    ]
+
+    # Subscribe the devices corresponding to the registration tokens to the
+    # topic.
+    response = messaging.subscribe_to_topic(registration_tokens, topic)
+    # See the TopicManagementResponse reference documentation
+    # for the contents of response.
+    print(response.success_count, 'tokens were subscribed successfully')
+
+schedule.every().hour.do(fetch_data)
 
 while True:
     schedule.run_pending()
