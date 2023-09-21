2db37d7a6900549d0a0791ac71096273c557e587
sopipc167 <sopipc167@gmail.com>
2023-09-21 20:11:37 +0900

    dto수정및 피드백 반영

--- api/feedback/feedback.py
+++ api/feedback/feedback.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restx import Resource, Namespace, fields
 from database.database import Database
-from utils import feedbackDTO
+from utils.dto import feedbackDTO
 
 feedback = feedbackDTO.api
 @feedback.route("/<int:feedback_code>")
@@ -31,7 +31,6 @@ class FeedbackGetAPI(Resource):  # 임원만(id) 볼 수 있어야함
             return feedback, 200
 
     def post(self, feedback_code):
-
         # Body 데이터 얻어오기
         body_data = request.get_json()
         is_anony = body_data['is_anony']
@@ -40,17 +39,17 @@ class FeedbackGetAPI(Resource):  # 임원만(id) 볼 수 있어야함
         content = body_data['content']
         database = Database()
 
-        #피드백 개수를 세서 id를 할당함
+        # 피드백 개수를 세서 id를 할당함
         sql = f"SELECT COUNT(code) from feedback;"
         count = database.execute(sql)
         feedback_code = count
 
-        #만약 작성항목에 널값이 있는지 확인
-        if (not title): #피드백 제목을 작성하지 않을때 예외 발생
+        # 만약 작성항목에 널값이 있는지 확인
+        if (not title): # 피드백 제목을 작성하지 않을때 예외 발생
             return {"message":"제목을 입력해주세요"}, 400
-        elif (not content): #피드백 내용을 작성하지 않을때 예외 발생
+        elif (not content): # 피드백 내용을 작성하지 않을때 예외 발생
             return {"message":"피드백 내용을 입력해주세요"}, 400
-        else:#피드백을 정상적으로 작성하고
+        else:# 피드백을 정상적으로 작성하고
             sql = f"INSERT INTO feedback(code, user_id, is_anony, title, content, is_answered) " \
                   f"VALUES('{feedback_code}', '{user_id}' ,{is_anony}, '{title}', '{content}', 0);"
             database.execute(sql)
@@ -79,7 +78,6 @@ class FeedbackListGetAPI(Resource):
         if not feedback_list:
             return {'message':'생성된 피드백이 하나도 없어요'}, 200
         else:
-            print(feedback_list)
             for idx, value in enumerate(feedback_list):
                 if feedback_list[idx]['is_anony'] == 1:  # 익명처리
                     feedback_list[idx]['user_id'] = 0
@@ -101,7 +99,7 @@ class FeedbackAnswerAPI(Resource):
 
         # 임원이 아닐 경우
         if not answer_id:
-            return {'message': '피드백을 볼 수 있는 권한이 없습니다'}, 401
+            return {'message': '피드백을 볼 수 있는 권한이 없어요'}, 401
 
         # feedback이 존재하는지 확인
         database = Database()
@@ -112,17 +110,16 @@ class FeedbackAnswerAPI(Resource):
         # feedback이 존재하지 않을 때의 처리
         if not feedback:
             database.close()
-            return {'message': '존재하지 않는 피드백입니다.'}, 400
+            return {'message': '존재하지 않는 피드백이에요.'}, 400
 
         # answer이 존재하는지 확인
-        database = Database()
         sql = f"SELECT * FROM feedback_answer WHERE code = '{feedback_code}';"
         feedback_answer = database.execute_one(sql)
 
         # answer이 존재할 때 처리
         if feedback_answer:
             database.close()
-            return {'message': '이미 답변된 피드백입니다.'}, 400
+            return {'message': '이미 답변된 피드백이에요.'}, 400
 
         # answer 작성
         else:
@@ -138,4 +135,4 @@ class FeedbackAnswerAPI(Resource):
             database.commit()
 
             database.close()
-            return {"피드백이 성공적으로 작성되었습니다"}, 200
+            return {"피드백이 성공적으로 작성되었어요"}, 200
