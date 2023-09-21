1f9cd67d5c231a0cd87305cf5b890814e8f23bcf
sopipc167 <sopipc167@gmail.com>
2023-09-21 21:20:06 +0900

    firebase sdk를 사용하도록 수정

--- FCM/FCMNotification.py
+++ FCM/FCMNotification.py
@@ -1,27 +1,23 @@
-from pyfcm import FCMNotification
-from database.database import Database
-from FCM import NotificationManager
+from firebase_admin import messaging
+from firebase_admin.messaging import Message,Notification
 import schedule
 import datetime
-import time
+import asyncio
 
-class Notification: #알림 객체
-    DAY_OF_WEEK = {'월요일':'monday','화요일':'tuesday','수요일':'wendsday','목요일':'thrsday','금요일':'frieday','토요일':'saturday','일요일':'sunday'} #알림의 요일을 영어로 바꾸기 위한 딕셔너리
+class fcmNotification(): #알림 객체
+    DAY_OF_WEEK ={0:'monday', 1:'tuesday',2:'wendsday',3:'thursday',4:'frieday',5:'saturday',6:'sunday'} #알림의 요일을 영어로 바꾸기 위한 딕셔너리
     scheduler = ""
     expire_day = datetime.datetime
     start_day = datetime.datetime
     week = ''
-    time = time.time()
+    time = datetime.timedelta
     cycle = ''
-    def __init__(self, data, apikey, token): #API Key와 앱 토큰을 초기화
+    def __init__(self, data, token): #API Key와 앱 토큰을 초기화
         self.DATA = data
-        APIKEY = apikey
-        TOKEN = token
-        push_service = FCMNotification(APIKEY)
-        self.timeParser(data)
+        self.TOKEN = token
+        self.setSchedule(data)
     def __del__(self):
-        print(f'{self.cycle} {self.time}시에 예약된 알람을 삭제했습니다.')
-        NotificationManager.delete_notification(self)
+        print(f'{self.DAY_OF_WEEK[self.week]} {self.time}시에 예약된 알람을 삭제했습니다.')
     def check_expired(self):
         if datetime.datetime.now() <= self.expire_day:
             return True
@@ -32,31 +28,54 @@ class Notification: #알림 객체
             self.__del__()
             return schedule.CancelJob
         # 메시지 (data 타입)
-        data_message = {
-            "body": body,
-            "title": title
-        }
-        result = self.push_service.single_device_data_message(registration_id=self.TOKEN, data_message=data_message)# 토큰값을 이용해 1명에게 푸시알림을 전송함
-        print(result) # 전송 결과 출력
-
+        message = Message(
+            notification=Notification(
+                title=title,
+                body=body
+            ),
+            token=self.TOKEN
+        )
+        result = messaging.send(message)# 토큰값을 이용해 1명에게 푸시알림을 전송함
+        return await result
+    async def sendtopicMessage(self,body, title,topic):
+        if self.check_expired():#만약 날짜가 만료되면 자동으로 스케쥴 취소
+            self.__del__()
+            return schedule.CancelJob
+        # 메시지 (data 타입)
+        message = Message(
+            notification=Notification(
+                title=title,
+                body=body
+            ),
+            topic=topic
+        )
+        result = messaging.send_each_for_multicast(message) #여러명에게 topic을 이용해 메세지 전송
+        return await result
+    async def runcoroutine(self,m,n):
+        aws = self.sendMessage(m,n)
+        await asyncio.gather(*aws)
+        print(aws)
     def startSchedule(self): #스케쥴을 시작함
-        eval(self.scheduler)
-
-    def setSchedule(self, data): #스케쥴을 설정하여 몇일, 몇주 주기로 메세지를 보낼지 결정
-        for i in data:
-            self.time=i['time']
-            self.week=i['day']
-            self.start_date=i['start_date']
-            self.expire_day=i['end_date']
-            self.cycle = i['cyle']
-            message=i['message']
-            memo=i['memo']
+        exec(f'asyncio.run({self.scheduler})')
+    def timeparser(self):
+        pass
+    def setSchedule(self, data): #스케쥴을 설정하여 몇일, 몇주 주기로 메세지를 보낼지 결정\
+        self.time=data['time']
+        self.week=data['day']
+        self.start_date=data['start_date']
+        self.expire_day=data['end_date']
+        self.cycle = data['cycle']
+        message=data['message']
+        memo=data['memo']
+        self.scheduler=  f"schedule.every(5).seconds.do(self.sendMessage,\"{message}\",\"{memo}\")"
+        pass
+        """
         if(self.cycle=='매주'):
-            self.scheduler = f"schedule.every().{self.DAY_OF_WEEK[self.week]}.at(\"{self.time}\").do(self.sendMessage,{message},{memo})"
+            self.scheduler = f"schedule.every().{self.DAY_OF_WEEK[self.week]}.at(\"{self.time}\").do(self.runcoroutine,\"{message}\",\"{memo}\")"
         elif(self.cycle=='매일'):
             self.scheduler = f"schedule.every().day.at(\"{time}\").do(self.sendMessage,{message},{memo})"
         else:
             self.scheduler = None
-
-
-
+        """
+    def test(self):
+        print('hello')
--- FCM/NotificationManager.py
+++ FCM/NotificationManager.py
@@ -1,14 +1,12 @@
 import schedule
 from database.database import Database
-from FCM.FCMNotification import Notification
-from pyfcm import FCMNotification
+from FCM.FCMNotification import fcmNotification
 import firebase_admin
+import asyncio
 from firebase_admin import credentials,messaging
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("violet-strike-firebase-adminsdk-2dlvt-8b8e680cb3.json")
 firebase_admin.initialize_app(cred)
-
-default_app = firebase_admin.initialize_app()
 
 FCM_LIST =[]
 def fetch_data(): #데이터베이스에서 알림 정보를 가져와서 알림 객체를 생성함
@@ -17,18 +15,21 @@ def fetch_data(): #데이터베이스에서 알림 정보를 가져와서 알림
     lst=database.execute_all(sql)
     database.close()
     for i in lst:
-        tmp=Notification(i,'unknown string wanted','unknown string wanted')
+        tmp=fcmNotification(i,'f0I4IU3XRuiWCsSEyuyubQ:APA91bEiQFBUtlAhLheP0hfvSnEsAgsLSUlIVAJADz-TgPDfqj9TW4-TzmMfnoBicUpyyB-QHLcExke7nrWHcfIo-7bCFfDCGybY9IulXFUV-Plbz8CH2gwR5gFTFf2Fm1m5ifplb4fn')
         tmp.startSchedule()
         FCM_LIST.append(tmp)
+    print(FCM_LIST)
 def delete_notification(n):
     FCM_LIST.remove(n)
 def register_topic(topic, registration_tokens): #FCM 토픽을 구독함
     response = messaging.subscribe_to_topic(registration_tokens, topic)
-    print(response.success_count, 'tokens were subscribed successfully')
+    print(response, '주제 구독을 성공적으로 완료함')
+
+def unregister_topic(topic, registration_tokens):
+    response = messaging.unsubscribe_from_topic(registration_tokens, topic)
+    print(response,'주제 구독해제를 성공적으로 완료함')
 
 schedule.every().hour.do(fetch_data)
-
+fetch_data()
 while True:
     schedule.run_pending()
-
-
