3f35a2ca5816cf9f51c027afb476831df8c9cf15
sopipc167 <sopipc167@gmail.com>
2023-10-05 21:39:41 +0900

    conflict 시도 3

--- api/admin/admin.py
+++ api/admin/admin.py
@@ -1,12 +1,16 @@
 from flask import Blueprint
 from flask_restx import Api
 from api.admin.accounting.accounting import accounting
+from api.admin.product.product import product
 from api.admin.notification.notification import notification
 from api.admin.attendance.attendance import attendance
+
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 api = Api(admin)
 
 api.add_namespace(accounting, '/accounting')
+api.add_namespace(product, '/product')
+api.add_namespace(product, '/product')
 api.add_namespace(notification, '/notification')
 api.add_namespace(attendance, '/attendance')
