d45ee0aaa15bbf625010567ef355b49a25d02f31
rhdehgks <rhdehgks@naver.com>
2023-11-06 20:06:02 +0900

    AccountingEnum 클래스 추가

--- utils/enum_tool.py
+++ utils/enum_tool.py
@@ -25,3 +25,8 @@ class NotificationEnum(EnumTool):
     NOTIFICATION_CATEGORY = {0: '디자인', 1: '아트', 2: '프로그래밍', 3: '정기', 4: '청소', 5: '기타'}
     MEMBER_CATEGORY = {0: '활동 중인 회원 전체', 1: '활동 중인 정회원', 2: '활동 중인 수습회원', 3: '기타 선택'}
     DAY_CATEGORY = {0: '월요일', 1: '화요일', 2: '수요일', 3: '목요일', 4: '금요일', 5: '토요일', 6: '일요일'}
+
+class AccountingEnum(EnumTool):
+    PAYMENT_STATE = {0: '환불 대상', 1: '기간 내 납입', 2: '조기 납입', 3: '체납', 4: '미입금'}
+    MEMBERSHIP_FEE_CATEGORY = {1: '납부 완료', 2: '납부 완료', 3: '납부 지연', 4: '미납'}
+    PAYMENT_METHOD = {0: '통장', 1: '금고'}
