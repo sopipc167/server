45f22cefe3bb77c8a2c5b82faa65af5da29c68f9
rhdehgks <rhdehgks@naver.com>
2023-11-06 16:40:56 +0900

    AttendanceEnum 클래스 추가

--- utils/enum_tool.py
+++ utils/enum_tool.py
@@ -15,4 +15,8 @@ class EnumTool:
 class UserEnum(EnumTool):
     RANK = {0: '탈퇴자', 1: '정회원', 2: '수습회원', 3: '명예회원', 4: '수습회원(휴학)', 5: '졸업생'}
     PART = {0: '디자인', 2: '아트', 3: '프로그래밍'}
-    REST_TYPE = {0: '일반휴학', 1:'군휴학'}
+    REST_TYPE = {-1: '활동', 0: '일반휴학', 1:'군휴학'}
+
+class AttendanceEnum(EnumTool):
+    CATEGORY = {0: '디자인', 1: '아트', 2: '프로그래밍', 3: '정기', 4: '기타'}
+    USER_ATTENDANCE_STATE = {0: '출석', 1: '지각', 2: '불참'}
