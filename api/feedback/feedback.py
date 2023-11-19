9c5b2bc9aed0ec7c9114893a43541fa2463cd7ec
sopipc167 <sopipc167@gmail.com>
2023-11-19 09:06:16 +0900

    피드백 코드 일부 수정

--- api/feedback/feedback.py
+++ api/feedback/feedback.py
@@ -1,11 +1,9 @@
 from flask import request
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, fields
 from database.database import Database
 from utils.dto import feedbackDTO
 
-feedback = Namespace('feedback')
-
-
+feedback = feedbackDTO.api
 @feedback.route("/<int:feedback_code>")
 @feedback.response(200, 'Success',feedbackDTO.feedback_response_search)
 @feedback.response(200, 'Post Success',feedbackDTO.post_sucess)
@@ -26,14 +24,13 @@ class FeedbackGetAPI(Resource):  # 임원만(id) 볼 수 있어야함
         # feedback이 하나도 없을 때의 처리
         if not feedback:
             database.close()
-            return {"message":"아직 피드백이 없어요."}, 200
+            return {"message":"존재하지 않는 피드백입니다"}, 400
         else:
             if feedback['is_anony'] == 1:  # 익명 처리
                 feedback['user_id'] = 0
             return feedback, 200
 
     def post(self, feedback_code):
-
         # Body 데이터 얻어오기
         body_data = request.get_json()
         is_anony = body_data['is_anony']
@@ -43,24 +40,24 @@ class FeedbackGetAPI(Resource):  # 임원만(id) 볼 수 있어야함
         database = Database()
 
         # 피드백 개수를 세서 id를 할당함
-        sql = f"select f.code from p_cube_plus.feedback as f order by code desc limit 1;"
-        feedback_code = database.execute(sql)
+        sql = f"SELECT COUNT(code) from feedback;"
+        count = database.execute(sql)
+        feedback_code = count
 
         # 만약 작성항목에 널값이 있는지 확인
-        if not title: # 피드백 제목을 작성하지 않을때 예외 발생
+        if (not title): # 피드백 제목을 작성하지 않을때 예외 발생
             return {"message":"제목을 입력해주세요"}, 400
-            database.close()
-        elif not content: # 피드백 내용을 작성하지 않을때 예외 발생
+        elif (not content): # 피드백 내용을 작성하지 않을때 예외 발생
             return {"message":"피드백 내용을 입력해주세요"}, 400
-            database.close()
-        else: # 피드백을 정상적으로 작성하고
+        else:# 피드백을 정상적으로 작성하고
             sql = f"INSERT INTO feedback(code, user_id, is_anony, title, content, is_answered) " \
-                  f"VALUES('{feedback_code['code']+1}', '{user_id}' ,{is_anony}, '{title}', '{content}', 0);"
+                  f"VALUES('{feedback_code}', '{user_id}' ,{is_anony}, '{title}', '{content}', 0);"
             database.execute(sql)
             database.commit()
 
             database.close()
             return body_data, 200
+
 
 @feedback.route("/list")
 @feedback.response(200, 'Success',feedbackDTO.feedback_response_all)
@@ -79,7 +76,7 @@ class FeedbackListGetAPI(Resource):
 
         # feedback이 하나도 없을 때의 처리
         if not feedback_list:
-            return {'message':'생성된 피드백이 하나도 없어요'}, 200
+            return {'message':'생성된 피드백이 없어요'}, 200
         else:
             for idx, value in enumerate(feedback_list):
                 if feedback_list[idx]['is_anony'] == 1:  # 익명처리
@@ -94,6 +91,7 @@ class FeedbackListGetAPI(Resource):
 @feedback.response(400,'already answered',feedbackDTO.already_answered)
 class FeedbackAnswerAPI(Resource):
     def post(self, feedback_code):
+
         # Body 데이터 얻어오기
         body_data = request.get_json()
         answer_id = body_data['user_id']
@@ -112,22 +110,21 @@ class FeedbackAnswerAPI(Resource):
         # feedback이 존재하지 않을 때의 처리
         if not feedback:
             database.close()
-            return {'message': '존재하지 않는 피드백이에요'}, 400
+            return {'message': '존재하지 않는 피드백이에요.'}, 400
 
         # answer이 존재하는지 확인
-        database = Database()
         sql = f"SELECT * FROM feedback_answer WHERE code = '{feedback_code}';"
         feedback_answer = database.execute_one(sql)
 
         # answer이 존재할 때 처리
         if feedback_answer:
             database.close()
-            return {'message': '이미 답변된 피드백이에요'}, 400
+            return {'message': '이미 답변된 피드백이에요.'}, 400
 
         # answer 작성
         else:
             if not answer:
-                return {"message":"답변 내용을 작성해주세요"}
+                return {"message":"답변 내용을 작성해주세요"}, 400
             sql = (f"INSERT INTO feedback_answer(code, user_id, answer, answer_id) " \
                    f"VALUES({feedback_code},'{user_id}' ,'{answer}', {answer_id});")
             database.execute(sql)
@@ -138,4 +135,4 @@ class FeedbackAnswerAPI(Resource):
             database.commit()
 
             database.close()
-            return {"피드백이 성공적으로 작성되었어요"}, 200
+            return {"message:피드백이 성공적으로 작성되었어요"}, 200
